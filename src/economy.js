fa63f12fa524765b3e781b4359c5f81e946814f8
radare <pancake@nopcode.org>
2012-05-09 09:57:53 +0200

    Add bitcoin transfer logic in economy

--- src/economy.js
+++ src/economy.js
@@ -1,3 +1,5 @@
+const donothing = false;
+
 var bitcoin = require ("bitcoin");
 var cfg = require ("./config").bitcoin;
 
@@ -5,6 +7,8 @@ var cli = new bitcoin.Client (cfg.host, cfg.port,
 	cfg.username, cfg.password);
 
 module.exports.getBalance = function (bid, cb) {
+	if (donothing)
+		return;
 	var name = "bug#"+bid;
 	cli.getBalance (name, function (e, d) {
 		// if address is not owned by the agent's wallet MUST WARN
@@ -14,14 +18,20 @@ module.exports.getBalance = function (bid, cb) {
 }
 
 module.exports.createBitcoinAddress = function (bn, cb) {
-	if (true) {
-		cli.getNewAddress ('bug#'+bn, function (e, d) {
-			console.log ("New Address has been done", d);
-			if (cb) cb (d);
-		});
-	} else cb (999);
+	if (donothing)
+		return;
+	cli.getNewAddress ('bug#'+bn, function (e, d) {
+		console.log ("New Address has been done", d);
+		if (cb) Process.nextTick (cb (d));
+	});
 }
 
-module.exports.transfer = function (from, to, amount) {
-	// TODO
+module.exports.transfer = function (from, to, amount, cb) {
+	if (donothing)
+		return;
+	// sendfrom <fromaccount> <tobitcoinaddress> <amount> 
+	// [minconf=1] [comment] [comment-to]
+	cli.sendFrom (from, to, amount, function (x) {
+		if (cb) Process.nextTick (cb);
+	});
 }
